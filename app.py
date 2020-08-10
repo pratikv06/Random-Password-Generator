211f72e4b1e8c77c6bb86f73e2dc278da27bc384
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-08-10 19:06:22 +0530

    Simple password Generator

---
+++ app.py
@@ -0,0 +1,45 @@
+import functions as func
+from tkinter import *
+from PIL import ImageTk, Image
+
+# Description
+root = Tk()
+root.title("Password Generator")
+root.iconbitmap(r'.\images\favicon.ico')
+root.resizable(width=False, height=False)
+root.geometry("248x232")
+
+# App logo
+img = ImageTk.PhotoImage(Image.open(".\images\Logo.png"))
+logo_panel = Label(root, image = img)
+logo_panel.grid(row=0, column=0, columnspan=4, sticky=W+E)
+
+# App Label
+applabel = Label(root, text = "Password Generator", font = 24)
+applabel.grid(row=1, column=0, columnspan=4, sticky=W+E)
+
+# Defining Radio
+var = IntVar()
+btn_poor = Radiobutton(root, text = "Poor", variable = var, value = 1, anchor=W)
+btn_avg = Radiobutton(root, text = "Average", variable = var, value = 2, anchor=W)
+btn_strong = Radiobutton(root, text = "Strong", variable = var, value = 3, anchor=W)
+
+# Display Radio
+btn_poor.grid(row=2, column=1, padx=10)
+btn_avg.grid(row=2, column=2, padx=10)
+btn_strong.grid(row=2, column=3, padx=10)
+
+# Submit Button
+btn_submit = Button(root, text="Generate", font="Helvetica 10 bold", padx=15, 
+                    command=lambda: func.button_click(var, result))
+btn_submit.grid(row=3, column=1, columnspan=3)
+
+# Display Password
+result = Entry(root, width=30, borderwidth=2)
+result.grid(row=4, column=1, columnspan=4, pady=5)
+
+# Status bar
+status = Label(root, text="Developed By Pratik, V1.0", bd=2, relief=SUNKEN, anchor=E)
+status.grid(row=5, column=0, columnspan=4, sticky=W+E)
+
+root.mainloop()
---
+++ functions.py
@@ -0,0 +1,26 @@
+import string
+import random
+
+def generate_random_password(pwd_length, result):
+    ''' Generating Password '''
+    str = string.ascii_letters + string.digits + "@#$%*&-_"
+    generated_password = ''.join(random.choices(str, k=pwd_length))
+    update_password(generated_password, result)
+
+def button_click(var, result):
+    ''' Selecting type of Password '''
+    user_choice = var.get()
+    if user_choice == 1:
+        pwd_length = random.randint(6, 8)
+    elif user_choice == 2:
+        pwd_length = random.randint(9, 12)
+    elif user_choice == 3:
+        pwd_length = random.randint(13,16)
+    else:
+        pwd_length = 10
+    generated_password = generate_random_password(pwd_length, result)
+
+def update_password(generated_password, result):
+    ''' Displaying password on app '''
+    result.delete('0','end')
+    result.insert(0, generated_password)
